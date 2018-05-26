5a4184fdfc4f0dd36f932d001b1a9e647b28484e
evanlal <el17604@my.bristol.ac.uk>
2018-05-26 11:17:11 +0100

    conn pool mysql

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
