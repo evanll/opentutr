106686a9e00e6a090e4fce4f1448964089c723d5
evanlal <el17604@my.bristol.ac.uk>
2018-05-26 11:34:24 +0100

    conn pool mysql 3

--- db.js
+++ db.js
@@ -3,43 +3,12 @@ require("dotenv").config();
 
 var mysql = require("mysql");
 
-var pool = mysql.createPool({
+var connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
-
-var connection = (function() {
-  function _query(query, params, callback) {
-    pool.getConnection(function(err, connection) {
-      if (err) {
-        connection.release();
-        callback(null, err);
-        throw err;
-      }
-
-      connection.query(query, params, function(err, rows) {
-        connection.release();
-        if (!err) {
-          callback(rows);
-        } else {
-          callback(null, err);
-        }
-      });
-
-      connection.on("error", function(err) {
-        connection.release();
-        callback(null, err);
-        throw err;
-      });
-    });
-  }
-
-  return {
-    query: _query
-  };
-})();
 
 // todo: remove
 // keep connection alive
